28a4e164343fc7be53baae6e55b17902712e1af5
Raven <ravenlimzhexuan1@gmail.com>
2024-01-05 12:59:18 +0800

    Moved firebase auth and UI to load before body

--- public/firebase/config.js
+++ public/firebase/config.js
@@ -1,32 +1,29 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+const loadEl = document.querySelector('#load');
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+// // The Firebase SDK is initialized and available here!
+//
+// firebase.auth().onAuthStateChanged(user => { });
+// firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+// firebase.firestore().doc('/foo/bar').get().then(() => { });
+// firebase.functions().httpsCallable('yourFunction')().then(() => { });
+// firebase.messaging().requestPermission().then(() => { });
+// firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+// firebase.analytics(); // call to activate
+// firebase.analytics().logEvent('tutorial_completed');
+// firebase.performance(); // call to activate
+//
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
-    const loadEl = document.querySelector('#load');
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-    // // The Firebase SDK is initialized and available here!
-    //
-    // firebase.auth().onAuthStateChanged(user => { });
-    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-    // firebase.firestore().doc('/foo/bar').get().then(() => { });
-    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
-    // firebase.messaging().requestPermission().then(() => { });
-    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-    // firebase.analytics(); // call to activate
-    // firebase.analytics().logEvent('tutorial_completed');
-    // firebase.performance(); // call to activate
-    //
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+try {
+    let app = firebase.app();
+    let features = [
+        'auth',
+        'firestore',
+    ].filter(feature => typeof app[feature] === 'function');
+    loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+} catch (e) {
+    console.error(e);
+    loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+}
 
-    try {
-        let app = firebase.app();
-        let features = [
-            'auth',
-            'firestore',
-        ].filter(feature => typeof app[feature] === 'function');
-        loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
-    } catch (e) {
-        console.error(e);
-        loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
-    }
-
-});
